08669d192ff80e5d883952c049e6308ce42dab38
Adrian Lubinski <la.adrianlubinski@gmail.com>
2024-12-11 19:45:33 -0500

    Add bondaries crack_me

---
+++ bondaries/keygen.py
@@ -0,0 +1,13 @@
+PW_BUFFER_SIZE = 64     
+PW_LEN_MIN = 38         # without /n/0
+PW_LEN_MAX = 47         # without /n/0
+
+def password_lookup(pw_buffer):
+    pw_len, end_len, i, rc = 0
+    beg_buf = ' ' * 4
+    
+
+
+if __name__ == '__main__':
+    pw_buffer = [0] * (PW_LEN_MAX + 1)
+
